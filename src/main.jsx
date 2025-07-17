b657c532dae643cc333a2fba46737973f6b89952
cosuoji <qelvinz@gmail.com>
2025-07-17 11:37:57 +0100

    redirects added

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import 'leaflet/dist/leaflet.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App /> 
     <Toaster toastOptions={{
         style: { border: '1px solid #1E3A8A', padding: '9px', color: '#1F2937' },
         success: { iconTheme: { primary: "#FFD700", secondary: "#1F2937" }},
